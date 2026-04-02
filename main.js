7c043b27daf442737f8a436738676655071a8195
Olamilekan-oluwayomi <ilesanmiolamilekan05@gmail.com>
2026-04-02 22:19:07 +0100

    fix: remove stray /> from index.html

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".main--header");
 
@@ -9,15 +8,3 @@ btnNavEl.addEventListener("click", function () {
 
   headerEl.classList.toggle("nav--open");
 });
-=======
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".main--header");
-
-console.log(headerEl);
-
-btnNavEl.addEventListener("click", function () {
-  console.log("hello");
-
-  headerEl.classList.toggle("nav--open");
-});
->>>>>>> c1710fbd41b703d6ede3cbdb698d509fbd07e51f
